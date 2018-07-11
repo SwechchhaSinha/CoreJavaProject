33ea0e6641a52071c0d90c8ce27929e6496f25f3
shubham96gupta <shubhamgupta71296@gmail.com>
2018-07-11 17:06:48 +0530

    Employee Pojo Done.

--- CafetariaManagementSystemCoreJava/src/com/cafe/beans/Employee.java
+++ CafetariaManagementSystemCoreJava/src/com/cafe/beans/Employee.java
@@ -1,5 +1,58 @@
 package com.cafe.beans;
 //Employee POJO
-public class Employee {
-
+public class Employee 
+{
+	private String EIN;
+	private String employeeName;
+	private char hasOpted;
+	private int monthlyFoodExpense;
+	private String password;
+	public Employee()
+	{
+		
+	}
+	public Employee(String eIN, String employeeName, char hasOpted, int monthlyFoodExpense, String password) {
+		super();
+		EIN = eIN;
+		this.employeeName = employeeName;
+		this.hasOpted = hasOpted;
+		this.monthlyFoodExpense = monthlyFoodExpense;
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Employee [EIN=" + EIN + ", employeeName=" + employeeName + ", hasOpted=" + hasOpted
+				+ ", monthlyFoodExpense=" + monthlyFoodExpense + ", password=" + password + "]";
+	}
+	public String getEIN() {
+		return EIN;
+	}
+	public void setEIN(String eIN) {
+		EIN = eIN;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public char getHasOpted() {
+		return hasOpted;
+	}
+	public void setHasOpted(char hasOpted) {
+		this.hasOpted = hasOpted;
+	}
+	public int getMonthlyFoodExpense() {
+		return monthlyFoodExpense;
+	}
+	public void setMonthlyFoodExpense(int monthlyFoodExpense) {
+		this.monthlyFoodExpense = monthlyFoodExpense;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
