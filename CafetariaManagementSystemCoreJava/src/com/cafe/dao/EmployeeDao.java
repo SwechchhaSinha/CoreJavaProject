b199e06b116d63ad85ae68cd8d6453536fff672f
SwechchhaSinha <sinhaswechchha07@gmail.com>
2018-07-11 18:49:10 +0530

    searchEmployee and updateEmployee functions in employeeDapImp

--- CafetariaManagementSystemCoreJava/src/com/cafe/dao/EmployeeDao.java
+++ CafetariaManagementSystemCoreJava/src/com/cafe/dao/EmployeeDao.java
@@ -8,8 +8,8 @@ import com.cafe.beans.Employee;
 public interface EmployeeDao
 {
 	List<Employee> listAllEmployees() throws ClassNotFoundException, SQLException;
-	Employee searchEmployee(String id);
+	Employee searchEmployee(String id) throws ClassNotFoundException, SQLException;
 	boolean insertEmployee(Employee employee);
-	boolean updateEmployee(String columnName,String value);
-	boolean updateEmployee(String columnName, int value);
+	boolean updateEmployee(String ein,String value) throws SQLException, ClassNotFoundException;
+	boolean updateEmployee(String columnName, int value) throws SQLException, ClassNotFoundException;
 }
