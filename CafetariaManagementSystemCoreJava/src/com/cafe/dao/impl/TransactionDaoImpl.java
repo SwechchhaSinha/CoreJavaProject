6e1b8550a6c29901761340819030f7491bfe95e2
SwechchhaSinha <sinhaswechchha07@gmail.com>
2018-07-12 17:54:02 +0530

    TransactionDaoImpl

---
+++ CafetariaManagementSystemCoreJava/src/com/cafe/dao/impl/TransactionDaoImpl.java
@@ -0,0 +1,53 @@
+package com.cafe.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.cafe.beans.Food;
+import com.cafe.beans.Transaction;
+import com.cafe.dao.TransactionDao;
+import com.cafe.helper.ConnectionHelper;
+
+public class TransactionDaoImpl implements TransactionDao{
+
+	@Override
+	public boolean insertTransaction(Transaction transaction) throws ClassNotFoundException, SQLException {
+		Connection conn = ConnectionHelper.getConnection();
+		PreparedStatement statement = conn.prepareStatement("Insert into transaction" + "values(?,?,?,?)");
+
+		statement.setString(1, transaction.getDate());
+		statement.setString(2, transaction.getFood_id());
+		statement.setInt(3, transaction.getQuantity());
+		statement.setInt(4, transaction.getPrice());
+
+		int rows = statement.executeUpdate();
+
+		if (rows <= 0)
+			return false;
+		else
+			return true;
+	}
+
+	
+
+	@Override
+	public Transaction searchtransaction(String transaction_date) throws ClassNotFoundException, SQLException {
+		Connection conn = ConnectionHelper.getConnection();
+		PreparedStatement statement = conn.prepareStatement("Select * from transaction where transaction_date=?");
+		statement.setString(0, transaction_date);
+		Transaction transaction = null;
+		ResultSet rs = statement.executeQuery();
+		while (rs.next()) {
+			String foodId = rs.getString(2);
+			int foodQuantity = rs.getInt(3);
+			int totalPrice=rs.getInt(4);
+
+			transaction = new Transaction(transaction_date,foodId,foodQuantity,totalPrice);
+
+		}
+		return transaction;
+	}
+
+}
