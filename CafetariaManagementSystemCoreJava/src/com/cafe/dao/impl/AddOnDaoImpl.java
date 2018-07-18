a8aa9f9794e4e805c7d46942d2a779d1dc6be12b
SwechchhaSinha <sinhaswechchha07@gmail.com>
2018-07-18 19:15:45 +0530

    changes for improvement

--- CafetariaManagementSystemCoreJava/src/com/cafe/dao/impl/AddOnDaoImpl.java
+++ CafetariaManagementSystemCoreJava/src/com/cafe/dao/impl/AddOnDaoImpl.java
@@ -128,15 +128,18 @@ public class AddOnDaoImpl implements AddOnDao{
 	{
 		Connection conn = ConnectionHelper.getConnection();
 		PreparedStatement statement = conn.prepareStatement("Update addOn set addonquantity=? where addOnId=?");
-		statement.setInt(1,(searchAddOn(addOnId).getAddOnQuantity())-quantity);
+		AddOn a=searchAddOn(addOnId);
+//		if(a.equals(null))
+//			return false;
+//		else
+//		{
+		statement.setInt(1,(a.getAddOnQuantity())-quantity);
 		statement.setString(2, addOnId);
 
 		int rows = statement.executeUpdate();
+		return true;
+		//}
 
-		if (rows <= 0)
-			return false;
-		else
-			return true;
 	}
 
 }
